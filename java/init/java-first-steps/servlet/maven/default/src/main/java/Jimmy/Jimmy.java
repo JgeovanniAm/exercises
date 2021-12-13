9ed42a135ff0c975dafc109925410df8b487c491
JgeovanniAm <geovanniam171078@gmail.com>
2021-12-12 22:44:39 -0600

    servlet

---
+++ java/init/java-first-steps/servlet/maven/default/src/main/java/Jimmy/Jimmy.java
@@ -0,0 +1,8 @@
+
+public class Jimmy {	
+	public static void main(String arg[]){
+		String greeks = "hello jimmy";
+		greeks = "value changed";
+		System.out.print("print from Main class" + "  " + greeks);
+	}
+}
